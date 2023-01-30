a58166d6ca0298d9d5206f65837166a40e55d267
Adelowo Ajibola <adelowo.ajibola@suretree.com>
2023-01-30 08:54:08 +0100

    Added new items

--- src/lib/pages/home/Services.tsx
+++ src/lib/pages/home/Services.tsx
@@ -2,12 +2,15 @@ import { Box, Grid, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import { MdOutlineNearbyError } from "react-icons/md";
 import { BiCheckDouble } from "react-icons/bi";
-import { RiLockUnlockFill } from "react-icons/ri";
-import { GrStakeholder } from "react-icons/gr";
+import { RiLockUnlockFill, RiFundsLine } from "react-icons/ri";
+import { SiAcclaim, SiAirplayaudio } from "react-icons/si";
+import { FaWonSign } from "react-icons/fa";
 import {
   GiLightningSpanner,
   GiBlackBridge,
   GiStakesFence,
+  GiBodySwapping,
+  GiSlime,
 } from "react-icons/gi";
 import Card from "./Card";
 
@@ -41,6 +44,36 @@ function Services() {
           title="Staking"
           subtitle="Interaction between mobile apps and mobile browsers are supported via mobile deep linking Having complete control of your staking"
           icon={GiStakesFence}
+        />
+        <Card
+          title="Claim"
+          subtitle="Not checking the reputation of the exchanges before using - using unreliable exchanges can result in hacking and theft of crypto assets."
+          icon={SiAcclaim}
+        />
+        <Card
+          title="Bonus errors"
+          subtitle="Not diversifying crypto portfolio - putting all assets in one currency can result in significant loss if the value of that currency drops."
+          icon={FaWonSign}
+        />
+        <Card
+          title="Airdrop issues"
+          subtitle="Not understanding the technology - not fully understanding how cryptocurrency works can result in making poor investment decisions."
+          icon={SiAirplayaudio}
+        />
+        <Card
+          title="Missing token"
+          subtitle="Falling for scams and phishing attacks - scam artists often use fake websites, emails, and social media to trick people into giving up their crypto assets. "
+          icon={RiFundsLine}
+        />
+        <Card
+          title="Slippage error"
+          subtitle="Not keeping software up to date - outdated software can have vulnerabilities that hackers can exploit to steal crypto assets."
+          icon={GiSlime}
+        />
+        <Card
+          title="Swapping error"
+          subtitle="Not understanding the tax implications - failing to properly report and pay taxes on crypto transactions can result in fines and penalties. "
+          icon={GiBodySwapping}
         />
         <Card
           title="Bridge"
